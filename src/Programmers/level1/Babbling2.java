096ddfcc98d47040a715da2af7ee1aa025b12790
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-03 21:18:59 +0900

    Babbling2 solution

--- src/Programmers/level1/Babbling2.java
+++ src/Programmers/level1/Babbling2.java
@@ -4,43 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Babbling2 {
-    public int solution(String[] babbling) {
-        int answer = 0;
-        String[] canSay = {"aya", "ye", "woo", "ma"};
-        String[] repeat = {"ayaaya", "yeye", "woowoo", "mama"};
-        boolean check;
-        String can;
+    private int answer = 0;
+    private String[] canSay = {"aya", "ye", "woo", "ma"};
+    private String[] repeat = {"ayaaya", "yeye", "woowoo", "mama"};
+    private boolean check;
+    private String can;
 
+    public int solution(String[] babbling) {
         for (int i = 0; i < babbling.length; i++) {
             check = true;
             can = "";
             for (int j = 0; j < canSay.length; j++) {
-                if (babbling[i].contains(canSay[j]))
-                    can += "O";
-                if (babbling[i].contains(repeat[j]))
-                    can += "X";
-                babbling[i] = babbling[i].replace(canSay[j], "");
+                can += containCheck(babbling[i], j);
+                babbling[i] = babbling[i].replace(canSay[j], "1");
             }
+            babbling[i] = babbling[i].replace("1", "");
             if (can.contains("X"))
                 check = false;
             if (check)
-                if (babbling[i].length() == 0)
-                    answer++;
-            System.out.println("answer = " + answer);
-            System.out.println("can = " + can);
+                answer += plusAnswer(babbling[i]);
         }
-
         return answer;
     }
 
-    /**
-     * 연속해서 나오는 경우만 발음하지 못할 뿐 떨어져 있다면 반응할 수 있다.
-     *
-     * "aya", "ye", "woo", "ma"
-     * 이렇게 4개를 통해 같은 단어가 연속되지 않게
-     * 만들 수 있는 개수
-     */
-    public static void main(String[] args) {
-        String babbling[] = {"aya", "yee", "u", "maa"};
+    public int plusAnswer(String babbling) {
+        if (babbling.length() == 0)
+            return 1;
+        return 0;
+    }
+
+    public String containCheck(String babbling, int idx) {
+        if (babbling.contains(repeat[idx]))
+            return "X";
+        if (babbling.contains(canSay[idx]))
+            return "O";
+        return "";
     }
 }
