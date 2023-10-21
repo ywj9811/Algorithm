5f66332fc8c7d2ef2de2c0261d0650dfb7e34c5d
yang <ywj9811@gmail.com>
2023-10-21 23:37:15 +0900

    feat: 알고리즘 풀이

---
+++ src/SWEA/SW_1954.java
@@ -0,0 +1,24 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SW_1954 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int n = sc.nextInt();
+        }
+    }
+
+    static void getMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+    }
+}
